196ac492088a4e35b2c668c5347043aa3e6e7aac
Feroz Panwaskar <feroz.panwaskar@gmail.com>
2013-03-15 12:16:50 +0000

    We should specify specific excludes for dependencies (see http://burtbeckwith.com/blog/?p=1719)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,41 +22,39 @@ grails.project.dependency.resolution = {
 	}
 
     dependencies {
-			build ("org.apache.ivy:ivy:2.2.0") {
-				transitive = false
-			}
+        compile('commons-httpclient:commons-httpclient:3.1') {
+            excludes 'commons-codec', 'commons-logging', 'junit'
+        }
 
-			compile ('org.opensaml:opensaml:2.5.3') {
-				transitive = false
-			}
-			compile ('org.opensaml:xmltooling:1.3.4') {
-				transitive = false
-			}
-			compile ('org.apache.santuario:xmlsec:1.4.4') {
-				transitive = false
-			}
-			compile ('org.bouncycastle:bcprov-jdk15:1.45') {
-				transitive = false
-			}
-			compile ('org.opensaml:openws:1.4.4') {
-				transitive = false
-			}
-			compile('joda-time:joda-time:1.6.2') {
-				transitive = false
-			}
-			compile('commons-httpclient:commons-httpclient:3.1') {
-				transitive = false
-			}
-			compile('org.apache.velocity:velocity:1.7') {
-				transitive = false
-			}
-			compile ('ca.juliusdavies:not-yet-commons-ssl:0.3.9') {
-				transitive = false
-			}
-			compile ('org.owasp.esapi:esapi:2.0.1')
-			{
-				transitive = false
-			}
+        compile('ca.juliusdavies:not-yet-commons-ssl:0.3.9') {
+            excludes 'commons-httpclient', 'log4j'
+        }
+
+        compile('org.opensaml:opensaml:2.5.3') {
+            excludes 'commons-codec', 'commons-collections', 'commons-lang', 'esapi', 'jcip-annotations', 'jcl-over-slf4j', 'joda-time', 'jul-to-slf4j', 'junit', 'log4j-over-slf4j', 'logback-classic', 'openws', 'serializer', 'servlet-api', 'slf4j-api', 'spring-core', 'spring-mock', 'testng', 'velocity', 'xalan', 'xercesImpl', 'xml-apis', 'xml-resolver', 'xmlunit'
+        }
+
+        compile('org.opensaml:xmltooling:1.3.4') {
+            excludes 'bcprov-jdk15', 'commons-codec', 'jcip-annotations', 'jcl-over-slf4j', 'joda-time', 'jul-to-slf4j', 'junit', 'log4j-over-slf4j', 'logback-classic', 'not-yet-commons-ssl', 'serializer', 'slf4j-api', 'testng', 'xalan', 'xercesImpl', 'xml-apis', 'xml-resolver', 'xmlsec', 'xmlunit'
+        }
+
+        compile('org.apache.velocity:velocity:1.7') {
+            excludes 'ant', 'commons-collections', 'commons-lang', 'commons-logging', 'hsqldb', 'jdom', 'junit', 'log4j', 'logkit', 'oro', 'servlet-api', 'werken-xpath'
+        }
+
+        compile 'joda-time:joda-time:1.6.2'
+
+        compile('org.opensaml:openws:1.4.4') {
+            excludes 'commons-codec', 'commons-httpclient', 'jcip-annotations', 'jcl-over-slf4j', 'joda-time', 'jul-to-slf4j', 'junit', 'log4j-over-slf4j', 'logback-classic', 'serializer', 'servlet-api', 'slf4j-api', 'spring-core', 'spring-mock', 'testng', 'xalan', 'xercesImpl', 'xml-apis', 'xml-resolver', 'xmltooling', 'xmlunit'
+        }
+
+        compile 'org.bouncycastle:bcprov-jdk15:1.45'
+
+        compile 'org.apache.santuario:xmlsec:1.4.4'
+
+        compile('org.owasp.esapi:esapi:2.0.1') {
+            excludes 'antisamy', 'bsh-core', 'commons-beanutils-core', 'commons-collections', 'commons-configuration', 'commons-fileupload', 'commons-io', 'jsp-api', 'junit', 'log4j', 'servlet-api', 'xom'
+        }
     }
 
     plugins {
